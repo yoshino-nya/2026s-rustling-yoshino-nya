642877831438e790045f6f3a76a03796118351f0
yoshino-nya <wu_yi_@outlook.com>
2026-03-11 16:37:37 +0800

    finish: threads, clippy, coversions and macros

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Person::default();
+        }
+        let age = v[1].to_string().parse::<usize>();
+        Person {
+            name: match v[0] {
+                "" => return Person::default(),
+                _ => v[0].to_string(),
+            },
+            age: match age {
+                Ok(x) => x,
+                _ => return Person::default(),
+            },
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if v[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        Ok(Person {
+            name: v[0].to_string(),
+            age: match v[1].to_string().parse::<usize>() {
+                Ok(num) => num,
+                Err(e) => return Err(ParsePersonError::ParseInt(e)),
+            },
+        })
     }
 }
 
